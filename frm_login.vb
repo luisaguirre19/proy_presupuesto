70663e2bf9f4e0afe5415be3c0df074d0ea2c07d
luisaguirre19 <luisaguirre_19@yahoo.es>
2021-08-20 21:26:00 -0600

    2da

--- frm_login.vb
+++ frm_login.vb
@@ -2,4 +2,8 @@
     Private Sub frm_login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub btn_ingresar_Click(sender As Object, e As EventArgs) Handles btn_ingresar.Click
+
+    End Sub
 End Class
