4cf7e8ba5fd23946f919a9368991084d2c4586ff
Luis Aguirre <luisaguirre_19@yahoo.es>
2021-09-05 09:27:02 -0600

    conexion con BD y login

--- frm_login.vb
+++ frm_login.vb
@@ -1,9 +1,26 @@
 ﻿Public Class frm_login
-    Private Sub frm_login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
+    Dim func As New cls_eventos
+    Dim dt As New DataTable
 
     Private Sub btn_ingresar_Click(sender As Object, e As EventArgs) Handles btn_ingresar.Click
+        If datos_conexion("sp_presupuesto") = False Then
+            MsgBox("No hubo conexión con el SP favor revisar conexión con servidor")
+            Me.Dispose()
+        End If
 
+        func.envio_sp("@mail", Trim(txt_correo.Text))
+        func.envio_sp("@pass", Trim(txt_password.Text))
+
+        dt = func.mapeatabla()
+        If dt.Rows.Count > 0 Then
+            If dt.Rows(0)(0) = 1 Then
+                MsgBox("Si")
+            Else
+                MsgBox("No")
+            End If
+        Else
+            MsgBox("nada")
+        End If
+        func.con_close()
     End Sub
 End Class
